17b4b0670fe71ea81f5ee2c7c91344f865ca742e
apple <apple@appledeimac.local>
2018-12-04 09:43:17 +0800

    ..parperView

---
+++ WeChatCCircle/WeChatCCircle/Classes/Moment/View/MMImagePreviewView.swift
@@ -0,0 +1,21 @@
+//
+//  MMImagePreviewView.swift
+//  WeChatCCircle
+//
+//  Created by apple on 2018/12/4.
+//  Copyright © 2018年 apple. All rights reserved.
+//
+
+import UIKit
+
+class MMImagePreviewView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
